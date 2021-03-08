57c1b06a1550414db553d87943d683fd88e5f7de
Tahreem <syedatahreem.sta@gmail.com>
2021-03-08 22:20:27 +1100

    test phase2

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 const decodeReq = require('./decodeReq')
-const shows = require('./input');
-
-
-app.get('/', (req, res) => {
-
-  const result = decodeReq(shows)
-  res.json({
-    response : result
-  })
-  
-})
 
 app.use((req, res, next) => {
   bodyParser.json()(req, res, err => {
@@ -28,18 +17,25 @@ app.use((req, res, next) => {
   });
 });
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
+ 
+  res.json({mssg: 
+    'Please add a body to your request!'})
+})
 
+app.post('/', (req, res) => {
+ 
   try {
 
     const shows = req.body.payload
     const result = decodeReq(shows)
-
+    
     res.json({
       response : result
     })
 
   } catch(error) {
+    
     res.status(400).json({  
         "error": "Could not decode request: JSON parsing failed"
 
