e024c83835fbca2de032866ab217bf627390a56b
Tahreem <syedatahreem.sta@gmail.com>
2021-03-10 21:07:53 +1100

    restructed in form of testable code 2

--- server.js
+++ server.js
@@ -5,11 +5,26 @@ const postRequest = require('./routes/postRequest')
 const getRequest = require('./routes/getRequest')
 const checkError = require ('./error_handling/checkError')
 
-app.use(checkError);
+app.use((req, res, next) => {
+  bodyParser.json()(req, res, err => {
+      if (err) {
+          console.error(err);
+          return res.status(400).json({  
+            "error": "Could not decode request: JSON parsing failed"
+        })
+      }
 
-app.get('/', getRequest )
+      next();
+  });
+});
 
-app.post('/', postRequest)
+app.get('/', async (req,res) =>{   
+ getRequest(req,res);
+})
+
+app.post('/', async(req,res) =>{
+postRequest(req,res)
+})
 
 app.listen(port, () => {
   console.log(`App listening at ${port}`)
